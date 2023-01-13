29eeb594db96158f9c6735f2a697f72cf62e0d30
esojay5000 <esojay5000@yahoo.com>
2023-01-13 12:19:38 -0330

    added test functions to test file

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-def testing():
+def test_conv_emp_length():
     # firs test
+    pass
+
+def test_load_test_data():
+    pass
+
+def test_preprocessing():
+    pass
+
+def test_prediction():
     pass
